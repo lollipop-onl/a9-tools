b29c4a8912192de6b6b45c511cb6f2311c31d7bf
lollipop-onl <lollipop@simochee.net>
2022-02-21 00:03:11 +0900

    🔧 add TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./pages/**/*.tsx", "./components/**/*.tsx", "./hooks/**/*.ts"],
+  theme: {
+    fontFamily: {
+      body: ["Zen Kaku Gothic Antique", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
